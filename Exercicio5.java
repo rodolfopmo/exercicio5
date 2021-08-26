cd83ec3e19accb1c3a67d5e37114a8d1cf7fbedd
rodolfopmo <rodolfo.pmo@gmail.com>
2021-08-26 16:58:55 +0000

    Exercicio 5

---
+++ Exercicio5.java
@@ -0,0 +1,13 @@
+public class Exercicio5{
+    public static void main(String [] args){
+    int x = 58;
+    if(x%2==0){
+        x = x/2;
+    System.out.println("O valor de x é: " +x);
+    }
+        else{
+            x = 3 * x + 1;
+        System.out.println("O valor de x é: " +x);
+        }
+    }
+}
